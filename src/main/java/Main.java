1e17440d5e386ea8e6fe3856e467358fa5585239
Gabriel M. Costa <gmessiasc@gmail.com>
2024-07-08 00:13:55 -0300

    extract url path - initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,14 +4,10 @@ import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Logs from your program will appear here!");
-    Socket clientSocket = null;
-
-     try (var serverSocket = new ServerSocket(4221)) {
+     try (final ServerSocket serverSocket = new ServerSocket(4221)) {
        serverSocket.setReuseAddress(true);
-       clientSocket = serverSocket.accept();
-       System.out.println("accepted new connection");
-       clientSocket.getOutputStream().write(HttpUtils.OUTPUT_BYTE);
+       final Socket socket = serverSocket.accept();
+       EndpointHandler.getPath(socket);
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }

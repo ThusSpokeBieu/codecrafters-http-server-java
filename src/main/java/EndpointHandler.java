436c1c0040ea63b0a611e65765741670e8c8d2e8
Gabriel M. Costa <gmessiasc@gmail.com>
2024-07-08 00:15:02 -0300

    extract url path - initial

--- src/main/java/EndpointHandler.java
+++ src/main/java/EndpointHandler.java
@@ -1,18 +1,12 @@
-import java.io.IOException;
-import java.net.Socket;
-
-public class EndpointHandler {
-  public static void getPath(final Socket socket) {
-    try {
-      final var input = socket.getInputStream().readAllBytes();
-      final var str = new String(input);
-
-      System.out.println(str);
-
-      System.out.println("accepted new connection");
-      socket.getOutputStream().write(HttpUtils.OUTPUT_BYTE);
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    }
-  }
-}
+//import java.io.IOException;
+//import java.net.Socket;
+//
+//public class EndpointHandler {
+//  public static void getPath(final Socket socket) {
+//    try {
+//
+//    } catch (IOException e) {
+//      System.out.println("IOException: " + e.getMessage());
+//    }
+//  }
+//}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,13 @@ public class Main {
      try (final ServerSocket serverSocket = new ServerSocket(4221)) {
        serverSocket.setReuseAddress(true);
        final Socket socket = serverSocket.accept();
-       EndpointHandler.getPath(socket);
+       final var input = socket.getInputStream().readAllBytes();
+       final var str = new String(input);
+
+       System.out.println(str);
+
+       System.out.println("accepted new connection");
+       socket.getOutputStream().write(HttpUtils.OUTPUT_BYTE);
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
